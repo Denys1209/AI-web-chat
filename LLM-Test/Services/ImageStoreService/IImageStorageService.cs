d6ca848e33324e40bc511844606a345b863f677c
Black foxi <dentef767@gmail.com>
2026-08-13 18:59:48 +0100

    start episode 5

---
+++ LLM-Test/Services/ImageStoreService/IImageStorageService.cs
@@ -0,0 +1,10 @@
+﻿namespace LLM_Test.Services.ImageStoreService;
+
+public interface IImageStorageService
+{
+    public Task<string> SaveAsync(byte[] data, string mineType, CancellationToken cancellationToken);
+
+    public Task<byte[]> ReadAsync(string path, CancellationToken cancellationToken);
+
+    public Task DeleteAsync(string path, CancellationToken cancellationToken);
+}
---
+++ LLM-Test/Services/ImageStoreService/LocalImageStorageService.cs
@@ -0,0 +1,35 @@
+﻿
+namespace LLM_Test.Services.ImageStoreService;
+
+public class LocalImageStorageService : IImageStorageService
+{
+
+    private readonly string _rootPath;
+
+    private static readonly Dictionary<string, string> _mimeTypeToExtension = new()
+    {
+        { "image/jpeg", ".jpg" },
+        { "image/png", ".png" },
+        { "image/webp", ".webp" }
+    };
+
+    public LocalImageStorageService(IWebHostEnvironment env)
+    {
+        _rootPath = rootPath;
+    }
+
+    public Task DeleteAsync(string path, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<byte[]> ReadAsync(string path, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> SaveAsync(byte[] data, string mineType, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
